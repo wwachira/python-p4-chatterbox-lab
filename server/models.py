4cc0cd2a6a608f8d574c0f5e9adb7852c5f61e77
lizwachira <lizwanguwachira@gmail.com>
2024-07-07 05:54:17 +0300

    CRUD test pass

--- server/models.py
+++ server/models.py
@@ -12,3 +12,15 @@ class Message(db.Model, SerializerMixin):
     __tablename__ = 'messages'
 
     id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String)
+    username = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    def __repr__(self):
+        return f'<Message {self.body}, {self.username}>'
+    
+    
+    
+
+
